d18d386d15e763eda7fbccdb5d604da614062046
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-26 18:40:42 +0700

    membuat program

---
+++ Semester_1/Bab_1_Runtunan/Menggunakan_C/Soal_5/Program.c
@@ -0,0 +1,23 @@
+// Algoritma "konversi jarak"
+// {Algoritma ini digugnakan untuk mengkonversikan jarak tempuh seekor semut dalam satuan cm}
+
+// DEKLARASI Pustaka
+#include <stdio.h>
+
+// DEKLARASI Variable
+int jarakSemut = 261341;
+int km;
+int m;
+int cm;
+int sisaJarak;
+
+// ALGORITMA
+void main () {
+
+    km = jarakSemut / 100000;
+    sisaJarak = jarakSemut % 100000;
+    m = sisaJarak / 100;
+    cm = sisaJarak % 100;
+    printf("x = %i, ini berarti semut menempuh jarak %i + %i + %i", jarakSemut, km, m, cm);
+
+}
