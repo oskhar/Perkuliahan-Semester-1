58d5ea277315eb7396b7f81bbbe22a562eb54043
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-03-21 09:34:35 +0700

    Up ⭐

---
+++ Semester_2/11220910000042/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Program awal</title>
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <h1>PERTEMUAN I -- SISTEM PAKAR</h1>
+        </div>
+
+        <div style="display: flex;">
+        <div id="sidebar">
+            <h3>navigasi</h3>
+            <ul id="navmenu">
+                <li><a href="index.php" class="selected">Profile</a></li>
+                <li><a href="konten/galeri.php">Galeri</a></li>
+                <li><a href="konten/jadwal.php">Jadwal Kuliah</a></li>
+            </ul>
+        </div>
+
+        <div id="page">
+            Ini halaman utama
+            <h2>&nbsp</h2>
+        </div>
+        </div>
+
+        <div id="clear"></div>
+
+        <div id="footer">
+            <p>&copy; 2010</p>
+        </div>
+
+    </div>
+</body>
+</html>
--- Semester_2/11220910000042/konten/galeri.php
+++ Semester_2/11220910000042/konten/galeri.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Program awal</title>
 </head>
 <body>
@@ -17,15 +17,18 @@
         <div id="sidebar">
             <h3>navigasi</h3>
             <ul id="navmenu">
-                <li><a href="index.php" class="selected">Profile</a></li>
-                <li><a href="konten/galeri.php">Galeri</a></li>
-                <li><a href="konten/jadwal.php">Jadwal Kuliah</a></li>
+                <li><a href="../">Profile</a></li>
+                <li><a href="galeri.php" class="selected">Galeri</a></li>
+                <li><a href="jadwal.php">Jadwal Kuliah</a></li>
             </ul>
         </div>
 
         <div id="page">
-            Ini halaman utama
-            <h2>&nbsp</h2>
+
+            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
+            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
+            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
+            <p><img src="../images/gambar.png" alt="Ini gambar" width="145" height="117"></p>
         </div>
         </div>
 
